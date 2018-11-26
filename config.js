8a999008a9d9018cc4060472166abd285a4f2425
liaoqiao@corp.netease.com <liaoqiao@corp.netease.com>
2018-11-26 14:25:41 +0800

    readme add standard link

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 module.exports = {
-    'vue': 'https://github.com/lq782655835/standard-vue-project.git',
-    'electron': 'https://github.com/lq782655835/electron-vue-template.git'
+    vue: 'https://github.com/lq782655835/standard-vue-project.git',
+    electron: 'https://github.com/lq782655835/electron-vue-template.git',
+    website: 'https://github.com/lq782655835/official-website-project.git'
 }
